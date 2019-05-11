348f46dc45f416b1ceba25344aa6f9adfffc3a43
Azret Kenzhaliev <user.name>
2019-05-12 03:04:53 +0900

    added wiki rest

--- server/sample_rest.py
+++ server/sample_rest.py
@@ -5,6 +5,9 @@ from flask import Flask
 from flask import request
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 import googleapiclient.discovery
 import requests
@@ -30,36 +33,54 @@ def get_video_list():
 
     query = youtube.search().list(
         part="snippet",
-        q=data["query"]
+        q=data["query"],
+        maxResults=15
     )
 
     response = query.execute()
-
-    # print(data)
-    # print(json.dumps(response))
-
     return json.dumps(response)
+
 
 @app.route("/subtitles", methods=["POST"])
 def get_video_subtitles():
     data = request.get_json()
 
-    # r = requests.get(f"https://subtitles-for-youtube.p.rapidapi.com/subtitles/VrMHA3yX_QI?translated=None&type=None",
-    #                  headers={"X-RapidAPI-Host": "subtitles-for-youtube.p.rapidapi.com",
-    #                           "X-RapidAPI-Key": "042f3fd40bmsh299a3d264e6259ep190110jsnf075030f8fec"})
-
-
-    r = requests.get(f"https://subtitles-for-youtube.p.rapidapi.com/subtitles/{data['videoID']}?translated=None&type=None",
-                     headers={"X-RapidAPI-Host": "subtitles-for-youtube.p.rapidapi.com",
-                              "X-RapidAPI-Key": "042f3fd40bmsh299a3d264e6259ep190110jsnf075030f8fec"})
-
-
-    # print(data)
-    # print(r.json())
-
-    #r.json() = scripts.txt
-
+    r = requests.get(
+        "https://subtitles-for-youtube.p.rapidapi.com/subtitles/"
+        f"{data['videoID']}?translated=None&type=None",
+        headers={
+            "X-RapidAPI-Host": "subtitles-for-youtube.p.rapidapi.com",
+            "X-RapidAPI-Key":
+                "042f3fd40bmsh299a3d264e6259ep190110jsnf075030f8fec"})
     return json.dumps(r.json())
+
+
+@app.route("/wiki", methods=["POST"])
+def get_wiki():
+    data = request.get_json()
+    keyword = data["keyword"]
+
+    print(data)
+    print(keyword)
+
+    options = Options()
+    # options.add_argument("--headless")
+    # options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"),
+                              chrome_options=options)
+
+    driver.get("https://www.google.com/")
+
+    q = driver.find_element_by_name("q")
+    q.send_keys(keyword, Keys.ENTER)
+
+    knowledge_panel = driver.find_element_by_xpath(
+        '//div[contains(@class, "knowledge-panel")]')
+
+    code = knowledge_panel.get_attribute("innerHTML")
+    print(code)
+
+    return code
 
 
 if __name__ == '__main__':
