9eb85c0d1c845cdfca884090e75928f29ad135a4
Devloper.JS <jspark6815@daum.net>
2019-05-11 18:15:24 +0900

    Add Main UI

--- client/application/static/main.js
+++ client/application/static/main.js
@@ -1,12 +1,19 @@
 
-function postReq(){
+function postReq(searchQuery){
     $.ajax({
         method: "POST",
         url: "http://localhost:5000",
-        data: { name: "John", location: "Boston" }
+        json: { "query" : searchQuery }
     });
 }
-
+function keyDownEvent(e) {
+    if (e.keyCode == 13) {
+        var tb = document.getElementById("srch");
+        console.log(tb.value);
+        postReq();
+        location.href = 'lectureView';
+    }
+}
 function getReq(){
     $.getJSON("http://localhost:5000", function(data) {
     console.log(data)
