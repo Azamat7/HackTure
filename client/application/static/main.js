7c113320587bdbeb06241e423dd8cd4ca4d7dcbb
Devloper.JS <jspark6815@daum.net>
2019-05-11 17:26:40 +0900

    Call PostReq

--- client/application/static/main.js
+++ client/application/static/main.js
@@ -1,12 +1,17 @@
 
-function postReq(){
+function postReq(searchQuery){
     $.ajax({
         method: "POST",
         url: "http://localhost:5000",
-        data: { name: "John", location: "Boston" }
+        data: { query: searchQuery }
     });
 }
-
+function keyDownEvent(e) {
+    if (e.keyCode == 13) {
+        postReq(document.getElementById('srch').value);
+        // location.href="./lectureView.html";
+    }
+}
 function getReq(){
     $.getJSON("http://localhost:5000", function(data) {
     console.log(data)
