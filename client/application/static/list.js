5022ce9f8abff68b244a838c8072cbe697bc441b
Devloper.JS <jspark6815@daum.net>
2019-05-12 03:36:51 +0900

    Add function

--- client/application/static/list.js
+++ client/application/static/list.js
@@ -1,2 +1,8 @@
 dict = localStorage.getItem("searchResults");
 console.log(JSON.parse(dict));
+
+dict = JSON.parse(dict);
+
+list1 = document.getElementById("lst1");
+console.log(dict.items[0].snippet.thumbnails.default.url);
+list1.children[0].src = dict.items[0].snippet.thumbnails.default.url;
